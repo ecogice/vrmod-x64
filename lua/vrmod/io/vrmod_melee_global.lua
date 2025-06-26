e311ea7c89aedd2fb934cdf435995b3298f915ef
Abyss-c0re <dev@abyss-core.com>
2025-06-26 22:08:42 +0300

    cleaned up melee code

--- lua/vrmod/io/vrmod_melee_global.lua
+++ lua/vrmod/io/vrmod_melee_global.lua
@@ -1,36 +1,15 @@
 ----------------------------------------
--- VRMod Melee System - Extended
-----------------------------------------
+-- VRMod Melee System 
+-- CONVARS -----------------------------
+local cv_allowgunmelee = CreateConVar("vrmod_melee_gunmelee", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cv_meleeVelThreshold = CreateConVar("vrmod_melee_velthreshold", "2.0", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cv_meleeDamage = CreateConVar("vrmod_melee_damage", "15", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cv_meleeDelay = CreateConVar("vrmod_melee_delay", "0.01", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cl_usefist = CreateClientConVar("vrmod_melee_usefist", "1", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
+local cl_usekick = CreateClientConVar("vrmod_melee_usekick", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
+local cl_fistvisible = CreateClientConVar("vrmod_melee_fist_visible", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
+local cl_effectmodel = CreateClientConVar("vrmod_melee_fist_collisionmodel", "models/hunter/plates/plate.mdl", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
 -- UTILITIES ---------------------------
-local convars, convarValues = vrmod.GetConvars()
-local function IsPlayerInVR(ply)
-    return vrmod.IsPlayerInVR(ply)
-end
-
-local function GetRightHandVel(ply)
-    return vrmod.GetRightHandVelocity(ply):Length() / 40
-end
-
-local function GetLeftHandVel(ply)
-    return vrmod.GetLeftHandVelocity(ply):Length() / 40
-end
-
-local function GetRightHandVelocity(ply)
-    return vrmod.GetRightHandVelocity(ply)
-end
-
-local function GetLeftHandVelocity(ply)
-    return vrmod.GetLeftHandVelocity(ply)
-end
-
-local function GetRightHandPos(ply)
-    return vrmod.GetRightHandPos(ply)
-end
-
-local function GetLeftHandPos(ply)
-    return vrmod.GetLeftHandPos(ply)
-end
-
 local function CreateCollisionBox(pos, ang, model, visible)
     local ent = ents.CreateClientProp()
     ent:SetModel(model)
@@ -63,41 +42,23 @@ local function SendMeleeAttack(src, dir, speed)
     net.SendToServer()
 end
 
--- CONVARS -----------------------------
-local cv_allowgunmelee = CreateConVar("vrmelee_gunmelee", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_allowfist = CreateConVar("vrmelee_fist", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_allowkick = CreateConVar("vrmelee_kick", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_meleeVelThreshold = CreateConVar("vrmelee_velthreshold", "2.0", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_meleeDamage = CreateConVar("vrmelee_damage", "15", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_meleeDelay = CreateConVar("vrmelee_delay", "0.01", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cl_usegunmelee = CreateClientConVar("vrmelee_usegunmelee", "1", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local cl_usefist = CreateClientConVar("vrmelee_usefist", "1", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local cl_usekick = CreateClientConVar("vrmelee_usekick", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local cl_fisteffect = CreateClientConVar("vrmelee_fist_collision", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local cl_fistvisible = CreateClientConVar("vrmelee_fist_visible", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local cl_effectmodel = CreateClientConVar("vrmelee_fist_collisionmodel", "models/hunter/plates/plate.mdl", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE)
-local NextMeleeTime = 0
 -- CLIENTSIDE --------------------------
 if CLIENT then
     local meleeBoxes = {}
     local meleeBoxLifetime = 0.1
+    local NextMeleeTime = 0
     hook.Add("VRMod_Tracking", "VRMeleeAttacks", function()
         local ply = LocalPlayer()
-        if not IsValid(ply) or not ply:Alive() or not IsPlayerInVR(ply) then return end
+        if not IsValid(ply) or not ply:Alive() or not vrmod.IsPlayerInVR(ply) then return end
         if NextMeleeTime > CurTime() then return end
         local function IsHoldingProp(ply, hand)
-            if not IsValid(ply) then return false end
-            if ply ~= LocalPlayer() then -- we only track local player's hands clientside
-                return false
-            end
-
+            if ply ~= LocalPlayer() then return false end
             if hand == "left" then
                 return IsValid(g_VR.heldEntityLeft)
             elseif hand == "right" then
                 return IsValid(g_VR.heldEntityRight)
-            else
-                return false
             end
+            return false
         end
 
         local function TryAttack(pos, vel, useWeaponModel)
@@ -116,30 +77,28 @@ if CLIENT then
                 filter = ply
             })
 
-            if cl_fisteffect:GetBool() then
-                local model, ang
-                if useWeaponModel then
-                    local wep = ply:GetActiveWeapon()
-                    model = IsValid(wep) and wep:GetModel() or cl_effectmodel:GetString()
-                    ang = vrmod.GetRightHandAng(ply) or Angle(0, 0, 0)
-                else
-                    model = cl_effectmodel:GetString()
-                    ang = tr2.HitNormal:Angle()
-                end
-
-                local ent = CreateCollisionBox(src, ang, model, cl_fistvisible:GetBool())
-                table.insert(meleeBoxes, {
-                    ent = ent,
-                    time = CurTime()
-                })
+            local model, ang
+            if useWeaponModel then
+                local wep = ply:GetActiveWeapon()
+                model = IsValid(wep) and wep:GetModel() or cl_effectmodel:GetString()
+                ang = vrmod.GetRightHandAng(ply) or Angle(0, 0, 0)
+            else
+                model = cl_effectmodel:GetString()
+                ang = tr2.HitNormal:Angle()
             end
+
+            local ent = CreateCollisionBox(src, ang, model, cl_fistvisible:GetBool())
+            table.insert(meleeBoxes, {
+                ent = ent,
+                time = CurTime()
+            })
 
             if tr2.Hit then SendMeleeAttack(src, normVel, speed) end
         end
 
         -- Gun Melee
-        if cv_allowgunmelee:GetBool() and cl_usegunmelee:GetBool() then
-            local rightVel = GetRightHandVel(ply)
+        if cv_allowgunmelee:GetBool() then
+            local rightVel = vrmod.GetRightHandVelocity(ply):Length() / 40
             if rightVel >= cv_meleeVelThreshold:GetFloat() then
                 local vm = ply:GetViewModel()
                 if IsValid(vm) then
@@ -153,27 +112,27 @@ if CLIENT then
 
                     if tr.Hit then
                         NextMeleeTime = CurTime() + cv_meleeDelay:GetFloat()
-                        TryAttack(vm:GetPos(), GetRightHandVelocity(ply), true)
+                        TryAttack(vm:GetPos(), vrmod.GetRightHandVelocity(ply), true)
                     end
                 end
             end
         end
 
         -- Fist Melee
-        if cv_allowfist:GetBool() and cl_usefist:GetBool() then
-            local leftVel = GetLeftHandVel(ply)
-            local rightVel = GetRightHandVel(ply)
+        if cl_usefist:GetBool() then
+            local leftVel = vrmod.GetLeftHandVelocity(ply):Length() / 40
+            local rightVel = vrmod.GetRightHandVelocity(ply):Length() / 40
             if leftVel >= cv_meleeVelThreshold:GetFloat() and not IsHoldingProp(ply, "left") then
                 NextMeleeTime = CurTime() + cv_meleeDelay:GetFloat()
-                TryAttack(GetLeftHandPos(ply), GetLeftHandVelocity(ply), false)
+                TryAttack(vrmod.GetLeftHandPos(ply), vrmod.GetLeftHandVelocity(ply), false)
             elseif rightVel >= cv_meleeVelThreshold:GetFloat() and not IsHoldingProp(ply, "right") then
                 NextMeleeTime = CurTime() + cv_meleeDelay:GetFloat()
-                TryAttack(GetRightHandPos(ply), GetRightHandVelocity(ply), true)
+                TryAttack(vrmod.GetRightHandPos(ply), vrmod.GetRightHandVelocity(ply), true)
             end
         end
 
         -- Kick Melee
-        if cv_allowkick:GetBool() and cl_usekick:GetBool() then
+        if cl_usekick:GetBool() then
             local data = g_VR.net[ply:SteamID()]
             if not data or not data.lerpedFrame then return end
             local frame = data.lerpedFrame
@@ -196,13 +155,11 @@ if CLIENT then
                     filter = ply
                 })
 
-                if cl_fisteffect:GetBool() then
-                    local ent = CreateCollisionBox(src, tr2.HitNormal:Angle(), cl_effectmodel:GetString(), cl_fistvisible:GetBool())
-                    table.insert(meleeBoxes, {
-                        ent = ent,
-                        time = CurTime()
-                    })
-                end
+                local ent = CreateCollisionBox(src, tr2.HitNormal:Angle(), cl_effectmodel:GetString(), cl_fistvisible:GetBool())
+                table.insert(meleeBoxes, {
+                    ent = ent,
+                    time = CurTime()
+                })
 
                 if tr2.Hit then
                     NextMeleeTime = CurTime() + cv_meleeDelay:GetFloat()
@@ -237,13 +194,11 @@ if SERVER then
         local base = cv_meleeDamage:GetFloat()
         local scaled = math.Clamp((speed / 80) ^ 2, 0.1, 6.0)
         local damage = base * scaled * 0.1
-        --print("Raw Speed:", speed)
-        --print("Final Damage:", damage)
         ply:LagCompensation(true)
         ply:FireBullets({
             Attacker = ply,
             Damage = damage,
-            Force = damage, -- optional: use damage as force
+            Force = damage,
             Num = 1,
             Tracer = 0,
             Dir = dir,
